ba69e71b883cba047052c1a5555dc1ec254a8335
Дмитрий Карякин <375447955552dk@gmail.com>
2021-12-27 16:18:02 +0300

    Comlited

--- SendingEmail/Service.cs
+++ SendingEmail/Service.cs
@@ -8,6 +8,7 @@ namespace SendingEmail
 {
     public class Service
     {
+        //https://alekseev74.ru/lessons/show/aspnet-core-mvc/email-sending
         private readonly ILogger<Service> logger;
 
         public Service(ILogger<Service> logger)
@@ -23,14 +24,14 @@ namespace SendingEmail
                 System.Net.Mail.MailMessage message = new System.Net.Mail.MailMessage();
                 message.IsBodyHtml = true; //тело сообщения в формате HTML
                 message.From = new MailAddress("admin@mycompany.com", "Моя компания"); //отправитель сообщения
-                message.To.Add("mail@yandex.ru"); //адресат сообщения
+                message.To.Add("375447955552dk@gmail.com"); //адресат сообщения
                 message.Subject = "Сообщение от System.Net.Mail"; //тема сообщения
                 message.Body = "<div style=\"color: red;\">Сообщение от System.Net.Mail</div>"; //тело сообщения
-                message.Attachments.Add(new Attachment("... путь к файлу ...")); //добавить вложение к письму при необходимости
+                //message.Attachments.Add(new Attachment("... путь к файлу ...")); //добавить вложение к письму при необходимости
 
                 using (System.Net.Mail.SmtpClient client = new System.Net.Mail.SmtpClient("smtp.gmail.com")) //используем сервера Google
                 {
-                    client.Credentials = new NetworkCredential("mail@gmail.com", "secret"); //логин-пароль от аккаунта
+                    client.Credentials = new NetworkCredential("vovami6a@gmail.com", "???????????????"); //логин-пароль от аккаунта
                     client.Port = 587; //порт 587 либо 465
                     client.EnableSsl = true; //SSL обязательно
 
@@ -50,15 +51,15 @@ namespace SendingEmail
             try
             {
                 MimeMessage message = new MimeMessage();
-                message.From.Add(new MailboxAddress("Моя компания", "admin@mycompany.com")); //отправитель сообщения
-                message.To.Add(new MailboxAddress("mail@yandex.ru")); //адресат сообщения
+                message.From.Add(new MailboxAddress("Моя компания", "vovami6a@gmail.com")); //отправитель сообщения
+                message.To.Add(new MailboxAddress("375447955552dk@gmail.com")); //адресат сообщения
                 message.Subject = "Сообщение от MailKit"; //тема сообщения
                 message.Body = new BodyBuilder() { HtmlBody = "<div style=\"color: green;\">Сообщение от MailKit</div>" }.ToMessageBody(); //тело сообщения (так же в формате HTML)
 
                 using (MailKit.Net.Smtp.SmtpClient client = new MailKit.Net.Smtp.SmtpClient())
                 {
-                    client.Connect("smtp.gmail.com", 587, true); //либо использум порт 465
-                    client.Authenticate("mail@gmail.com", "secret"); //логин-пароль от аккаунта
+                    client.Connect("smtp.gmail.com", 465, true); //либо использум порт 465
+                    client.Authenticate("vovami6a@gmail.com", "??????????????????"); //логин-пароль от аккаунта
                     client.Send(message);
 
                     client.Disconnect(true);
